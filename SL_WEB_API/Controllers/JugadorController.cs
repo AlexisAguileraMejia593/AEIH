8c1eef67e9aaa87746773cd96dccae4d5fc3368f
EduSantiagoR <e.santiago.r05@gmail.com>
2023-12-26 13:42:09 -0600

    Jugador Controller Completo

---
+++ SL_WEB_API/Controllers/JugadorController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SL_WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JugadorController : ControllerBase
+    {
+        [Route("")]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<object> jugdores = BL.Jugador.GetAll();
+            return Ok(jugdores);
+        }
+        [Route("{idJugador}")]
+        [HttpGet]
+        public IActionResult GetById(int idJugador)
+        {
+            ML.Jugador jugador = BL.Jugador.GetById(idJugador);
+            if(jugador.Nacionalidad != null)
+            {
+                return Ok(jugador);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        [Route("")]
+        [HttpPost]
+        public IActionResult Add(ML.JugadorEquipo jugador)
+        {
+            bool result = BL.Jugador.Add(jugador);
+            if (result)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+        [Route("{idJugador}")]
+        [HttpPut]
+        public IActionResult Update(int idJugador, [FromBody]ML.JugadorEquipo jugador)
+        {
+            jugador.Jugador.IdJugador = idJugador;
+            bool result = BL.Jugador.Update(jugador);
+            if (result)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+        [Route("{idJugador}")]
+        [HttpDelete]
+        public IActionResult Delete(int idJugador)
+        {
+            bool result = BL.Jugador.Delete(idJugador);
+            if (result)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+    }
+}
